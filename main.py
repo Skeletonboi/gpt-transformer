eaeb5aa63b6ee0bc5a4b42f4c10c23a0d7a053d9
Skeletonboi <victor.yip@mail.utoronto.ca>
2024-07-15 22:21:30 -0700

    Updating training script

--- main.py
+++ main.py
@@ -20,38 +20,47 @@ with open("config.yaml") as file:
 device = "cpu"
 if torch.cuda.is_available():
     device = "cuda"
+
+# CONFIG PARAMS
+n_steps = config["n_steps"]
+lr = float(config["lr"])
+test_generate = config["test_generate"]
+batch_size = config["batch_size"]
+n_context = config["n_context"]
+train = config["train"]
+
 # TOKENIZER
 tokenizer = tiktoken.encoding_for_model('gpt2')
+
 # LOAD DATA
 dataset_name = "yahma/alpaca-cleaned"
 dataset_name = "GAIR/lima"
 dataset = load_dataset(dataset_name)
-dataloader = SimpleDataLoader(config["batch_size"], config["n_context"], dataset, \
+dataloader = SimpleDataLoader(batch_size, n_context, dataset, \
                                   dataset_name, tokenizer=tokenizer, device=device)
-# TRAINING
-steps = config["n_steps"]
-lr = float(config["lr"])
 
+# INIT MODEL
 model = GPT.from_pretrained('gpt2-large', device, use_flash_attn=True)
 # model = GPT(config)
 model.to(device)
 model = torch.compile(model)
 optimizer = torch.optim.AdamW(model.parameters(), lr=lr)
 
-for i in range(steps):
-    print(f"Training step {i}...")
-    x, y = dataloader.next_batch()
-    x, y = x.to(device), y.to(device)
-    optimizer.zero_grad()
-    with torch.autocast(device_type=device, dtype=torch.bfloat16):
-        logits, loss = model(x, y)
-    loss.backward()
-    optimizer.step()
+# TRAIN
+if train:
+    for i in range(n_steps):
+        print(f"Training step {i}...")
+        x, y = dataloader.next_batch()
+        x, y = x.to(device), y.to(device)
+        optimizer.zero_grad()
+        with torch.autocast(device_type=device, dtype=torch.bfloat16):
+            logits, loss = model(x, y)
+        loss.backward()
+        optimizer.step()
 
-test_generate = True
 torch.save(model.state_dict(), "./model.pth") 
 
-
+# GENERATE
 if test_generate:
     model.eval()
     out = generate_output("Tell me the purpose of my existence. ", model, tokenizer, device, \
