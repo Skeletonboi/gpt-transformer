d001463db609c7fefa55fc24c6305da898df1273
Skeletonboi <victor.yip@mail.utoronto.ca>
2024-07-17 00:01:54 -0700

    Updated main training/inference script to save original model params, and use LoRA

--- main.py
+++ main.py
@@ -2,11 +2,12 @@ import torch
 import torch.nn as nn
 import tiktoken
 import yaml
+from tqdm import tqdm
 from datasets import load_dataset
 # Custom libraries from scratch
 from transformer import GPT
 from dataloader import SimpleDataLoader
-from helper_functions import generate_output, replaceWithLoRA
+from helper_functions import generate_output, applyLoRA
 
 # LOAD CONFIG
 with open("config.yaml") as file:
@@ -27,10 +28,14 @@ lr = float(config["lr"])
 test_generate = config["test_generate"]
 batch_size = config["batch_size"]
 n_context = config["n_context"]
+
 train = config["train"]
-load_model = config["load_model"]
-model_path = config["model_path"]
 pretrained_name = config["pretrained_name"]
+save_model = config["save_model"]
+save_model_path = config["save_model_path"]
+
+load_model = config["load_model"]
+load_model_path = config["load_model_path"]
 lora_params = config["lora_params"]
 
 # TOKENIZER
@@ -46,25 +51,24 @@ dataloader = SimpleDataLoader(batch_size, n_context, dataset, \
 # INIT MODEL
 if load_model:
     model = GPT(config, device)
-    model = torch.compile(model)
-    model.load_state_dict(torch.load(model_path))
+    if lora_params["use_lora"]:
+        applyLoRA(model, lora_params, device)
+    model.to(device)
+    model.load_state_dict(torch.load(load_model_path))
 else:
     model = GPT.from_pretrained(pretrained_name, device, use_flash_attn=True)
-    model = torch.compile(model)
-model.to(device)
-import code; code.interact(local=locals())
-if lora_params["use_lora"]:
-    replaceWithLoRA(model, lora_params["replaced_modules"], lora_params["lora_rank"], lora_params["lora_alpha"])
-    for name, param in model.named_parameters():
-        if not any(target in name for target in lora_params):
-            param.requires_grad_(False)
-            
+    if lora_params["use_lora"]:
+        applyLoRA(model, lora_params, device)
+    model.to(device)
+
+model = torch.compile(model)
+
 optimizer = torch.optim.AdamW(model.parameters(), lr=lr)
 
 # TRAIN
 if train:
+    pbar = tqdm(total=n_steps, desc="Beginning training...")
     for i in range(n_steps):
-        print(f"Training step {i}...")
         x, y = dataloader.next_batch()
         x, y = x.to(device), y.to(device)
         optimizer.zero_grad()
@@ -72,13 +76,16 @@ if train:
             logits, loss = model(x, y)
         loss.backward()
         optimizer.step()
+        pbar.update(1)
+    pbar.close()
 
-# torch.save(model.state_dict(), "./model2.pth") 
+if save_model:
+    torch.save(model._orig_mod.state_dict(), save_model_path) 
 
 # GENERATE
 if test_generate:
     model.eval()
-    out = generate_output("Tell me the purpose of my existence. ", model, tokenizer, device, \
+    out = generate_output("What is the purpose of existence?", model, tokenizer, device, \
                         gen_length=100, num_samples=5, temp=1.0, top_k=50)
     for b in out:
         print("---GENERATED TEXT---")
