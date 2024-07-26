c5c89bb32259f44d34bb2b2b600fb8f48ee85152
Skeletonboi <victor.yip@mail.utoronto.ca>
2024-07-25 22:36:46 -0700

    Added classmethod to create training and test set dataloaders separately

--- dataloader.py
+++ dataloader.py
@@ -6,40 +6,60 @@ class SimpleDataLoader:
     Simple DataLoader for instruction-tuning datasets.
     Uses packed dataloading for efficient training.
     """
-    def __init__(self, batch_size, n_context, dataset, dataset_name, tokenizer, device):
+    def __init__(self, raw_data, batch_size, n_context, dataset_name, tokenizer, device):
         self.batch_size = batch_size
         self.n_context = n_context
         self.current_pos = 0
 
-        self.train = []
-        self.raw_train = dataset["train"]
-        self.n_samples = len(self.raw_train)
+        self.data = []
+        self.raw_data = raw_data
+        self.n_samples = len(self.raw_data)
         self.eos_token = tokenizer.eot_token
-        # Handling for different dataset classes from HuggingFace
+        # Converting raw dataset to tokenized array
         if dataset_name == "GAIR/lima":
-            self.raw_train = ["".join(sample) for sample in self.raw_train["conversations"]]
-            self.train.extend(tokenizer.encode(sample) + [self.eos_token] for sample in self.raw_train)
-            self.train = [token for sample in self.train for token in sample]
+            self.raw_data = ["".join(sample) for sample in self.raw_data["conversations"]]
+            self.data.extend(tokenizer.encode(sample) + [self.eos_token] for sample in self.raw_data)
+            self.data = [token for sample in self.data for token in sample]
         elif dataset_name == "yahma/alpaca-cleaned":
-            input = self.raw_train["input"]
-            output = self.raw_train["output"]
-            instr = self.raw_train["instruction"]
+            input = self.raw_data["input"]
+            output = self.raw_data["output"]
+            instr = self.raw_data["instruction"]
             for i in range(self.n_samples):
                 sample_str = "".join(instr[i]) + "".join(input[i]) + "".join(output[i])
-                self.train.extend(tokenizer.encode(sample_str)+[self.eos_token])
+                self.data.extend(tokenizer.encode(sample_str)+[self.eos_token])
 
         # Convert to tensor
-        self.train = torch.tensor(self.train, dtype=int)#.to_device(device) 
+        self.data = torch.tensor(self.data, dtype=int)#.to_device(device) 
         # removed to try to reduce memory usage, don't need whole dataset on device
         
     def next_batch(self):
-        if self.current_pos + self.batch_size * self.n_context + 1 > self.train.size(0):
+        if self.current_pos + self.batch_size * self.n_context + 1 > self.data.size(0):
             self.current_pos = 0
-        x = self.train[self.current_pos : self.current_pos + self.batch_size * self.n_context]
-        y = self.train[self.current_pos + 1 : self.current_pos + self.batch_size * self.n_context + 1]
+        x = self.data[self.current_pos : self.current_pos + self.batch_size * self.n_context]
+        y = self.data[self.current_pos + 1 : self.current_pos + self.batch_size * self.n_context + 1]
         x = x.view(self.batch_size, self.n_context)
         y = y.view(self.batch_size, self.n_context)
 
         self.current_pos += self.batch_size * self.n_context
         
         return x, y
+
+    def reset(self):
+        self.current_pos = 0
+        return
+
+    @classmethod
+    def createDataloader(cls,  dataset, batch_size, n_context, dataset_name, tokenizer, device):
+        # Separate train and test sets
+        if dataset.get("test", None):
+            raw_train = dataset["train"]
+            raw_test = dataset["test"]
+        else:
+            train_idxs = torch.randint(0, len(dataset["train"]), (int(len(dataset["train"])*0.95),))
+            test_idxs = [i for i in range(len(dataset["train"])) if i not in train_idxs] # this is slow O(n^2) - fix
+            raw_train = dataset["train"].select(train_idxs)
+            raw_test = dataset["train"].select(test_idxs)
+        # Create dataloaders
+        train_loader = SimpleDataLoader(raw_train, batch_size, n_context, dataset_name, tokenizer, device)
+        test_loader = SimpleDataLoader(raw_test, batch_size, n_context, dataset_name, tokenizer, device)
+        return train_loader, test_loader
