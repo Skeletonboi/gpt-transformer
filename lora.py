4de894de05c60a479d7ec966e55711991f7b63b3
Skeletonboi <victor.yip@mail.utoronto.ca>
2024-07-25 22:53:11 -0700

    Initial QDoRA implementation doesnt work

--- lora.py
+++ lora.py
@@ -49,12 +49,30 @@ class DoRALayer(nn.Module):
         super().__init__()
         self.linear = nn.Linear(module.in_features, module.out_features, bias=bias)
         self.lora = LowRankLayer(module.in_features, module.out_features, rank, alpha)
-        self.magnitude = nn.Parameter(module.weight.norm(p=2, dim=0, keepdim=True), requires_grad=True)
+        self.magnitude = nn.Parameter(module.weight.norm(p=2, dim=1, keepdim=True), requires_grad=True)
 
         self.linear.weight.requires_grad = False
 
     def forward(self, x):
         merged_weights = self.linear.weight + (self.lora.B.weight @ self.lora.A.weight) * self.lora.scale
-        merged_weights = merged_weights / merged_weights.norm(p=2, dim=0, keepdim=True)
+        merged_weights = merged_weights / merged_weights.norm(p=2, dim=1, keepdim=True)
+        merged_weights = self.magnitude * merged_weights
+        return  F.linear(x, merged_weights, bias=self.linear.bias)
+
+class QDoRALayer(nn.Module):
+    """
+    DoRA with pre-trained linear weights quantized to NF4
+    """
+    def __init__(self, module, rank, alpha, bias=True):
+        super().__init__()
+        self.linear = bnb.nn.LinearNF4(module.in_features, module.out_features, bias=bias)
+        self.lora = LowRankLayer(module.in_features, module.out_features, rank, alpha)
+        self.magnitude = nn.Parameter(module.weight.norm(p=2, dim=1, keepdim=True), requires_grad=True)
+
+        self.linear.weight.requires_grad = False
+
+    def forward(self, x):
+        merged_weights = self.linear.weight + (self.lora.B.weight @ self.lora.A.weight) * self.lora.scale
+        merged_weights = merged_weights / merged_weights.norm(p=2, dim=1, keepdim=True)
         merged_weights = self.magnitude * merged_weights
         return  F.linear(x, merged_weights, bias=self.linear.bias)
